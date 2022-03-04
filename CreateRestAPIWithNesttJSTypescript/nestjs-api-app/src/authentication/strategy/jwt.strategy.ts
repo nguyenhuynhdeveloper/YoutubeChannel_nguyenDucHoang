1177d15a27004a258cb16bfcdfa5fca0176d0522
Nguyen Duc Hoang <hoangnd@MacBook-Pro.local>
2022-03-04 16:23:52 +0700

    add code

--- CreateRestAPIWithNesttJSTypescript/nestjs-api-app/src/authentication/strategy/jwt.strategy.ts
+++ CreateRestAPIWithNesttJSTypescript/nestjs-api-app/src/authentication/strategy/jwt.strategy.ts
@@ -2,17 +2,27 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { DatabaseService } from "src/database/database.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
+    constructor(private configService: ConfigService, private databaseService: DatabaseService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey:configService.get('JWT_SECRET_KEY')
         })
     }
-    validate(payload: any) {
-        console.log({payload})
-        return payload
+    async validate(payload: {sub, email, iat }) {
+        //payload = decoded object from bearer token
+        console.log(`payload : ${JSON.stringify(payload)}`)
+        debugger
+        const user = await this.databaseService.user
+            .findUnique({
+                where: {
+                    id: payload.sub
+                }
+            })
+        delete user.hashPassword
+        return user
     }
 }
--- CreateRestAPIWithNesttJSTypescript/nestjs-api-app/src/user/user.controller.ts
+++ CreateRestAPIWithNesttJSTypescript/nestjs-api-app/src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from "express";
+import { MyJwtGuard } from 'src/authentication/guard';
 
 @Controller('user')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
+    //@UseGuards(AuthGuard('jwt'))
+    @UseGuards(MyJwtGuard)
     @Get('me')
     ///user/me
-    getMe() {        
-        return 'This is user/me'
+    getMe(@Req() request: Request) {        
+        //return 'This is user/me'
         //Guard will check the strategy before route handle
+        return request.user
     }
 }
