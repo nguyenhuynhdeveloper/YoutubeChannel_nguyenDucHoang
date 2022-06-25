9f6548dae8c703bd1c28b2960ccd5941b3b3adb3
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-06-25 09:54:25 +0700

    add code

--- Build an Infinite List with SwiftUI/code/InfiniteListApp/InfiniteListApp/InfiniteListAppApp.swift
+++ Build an Infinite List with SwiftUI/code/InfiniteListApp/InfiniteListApp/InfiniteListAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InfiniteListAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            InfiniteListView()
         }
     }
 }
--- Build an Infinite List with SwiftUI/code/InfiniteListApp/InfiniteListApp/Repositories/ItemRepository.swift
+++ Build an Infinite List with SwiftUI/code/InfiniteListApp/InfiniteListApp/Repositories/ItemRepository.swift
@@ -9,7 +9,8 @@ class ItemRepository:ObservableObject {
     private func urlGetProducts(startIndex:Int, limit:Int) -> String {
         "https://\(ItemRepository.SERVER)/photos?_start=\(startIndex)&_limit=\(limit)"
     }
-    @Published var products: [Item] = []
+    static let shared = ItemRepository()
+    @Published var items: [Item] = []
     @Published var isLoading = false
     @Published var hasReachedMax = false
     @Published var itemStatus:ItemStatus = .initial
@@ -21,25 +22,40 @@ class ItemRepository:ObservableObject {
             return
         }
         do {
-            guard let url = URL(string: urlGetProducts(startIndex: startIndex, limit: limit)) else {
-                isLoading = false
-                itemStatus = .failure(message: "Invalid url")
-                return
-            }
-            let sharedSession = URLSession.shared
             if itemStatus == ItemStatus.initial {
-                
+                let responseProducts = await self.fetchItems(startIndex: startIndex, limit: limit)
+                self.items = self.items + responseProducts
+                self.isLoading = false
+                self.itemStatus = .success
             }
-            let (data, response) = try await sharedSession.data(from: url, delegate: nil)
-            var responseProducts:[Item] = []
-            responseProducts = try JSONDecoder().decode([Item].self, from: data)
-            self.products = self.products + responseProducts
+            let responseProducts = await self.fetchItems(startIndex: items.count, limit: limit)
+            if responseProducts.isEmpty {
+                self.hasReachedMax = true
+            } else {
+                self.items = self.items + responseProducts
+                self.itemStatus = .success
+                self.hasReachedMax = false
+            }
             self.isLoading = false
         }catch {
             isLoading = false
             itemStatus = .failure(message: error.localizedDescription)
         }
     }
-    //private func fetchProduct
+    private func fetchItems(startIndex:Int, limit:Int) async -> [Item]{
+        do {
+            guard let url = URL(string: urlGetProducts(startIndex: startIndex, limit: limit)) else {
+                isLoading = false
+                itemStatus = .failure(message: "Invalid url")
+                return []
+            }
+            let sharedSession = URLSession.shared
+            let (data, response) = try await sharedSession.data(from: url, delegate: nil)
+            return try JSONDecoder().decode([Item].self, from: data)
+            
+        }catch {
+            return []
+        }
+    }
 }
 
