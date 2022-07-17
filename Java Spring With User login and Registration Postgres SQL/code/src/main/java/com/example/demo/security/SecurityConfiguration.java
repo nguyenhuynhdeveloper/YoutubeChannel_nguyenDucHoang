b1f702fe659ec1579bb5dbd711817de3d624f794
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-07-17 09:08:57 +0700

    add code

--- Java Spring With User login and Registration Postgres SQL/code/src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ Java Spring With User login and Registration Postgres SQL/code/src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,33 +11,46 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration  {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+                .hasAuthority("SCOPE_read")
+                .antMatchers(HttpMethod.POST, "/api/foos")
+                .hasAuthority("SCOPE_write")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
 
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
-//                .hasAnyAuthority("Admin", "Editor", "Salesperson")
-//                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
-                .anyRequest().authenticated()
-                .and().formLogin()
-                .loginPage("/login")
-                .usernameParameter("email")
-                .permitAll()
-                .and()
-                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
-                .and()
-                .logout().permitAll();
+//        http.authorizeRequests()
+//                .antMatchers("/login").permitAll()
+//                .antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
+////                .hasAnyAuthority("Admin", "Editor", "Salesperson")
+////                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+//                .anyRequest().authenticated()
+//                .and().formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("email")
+//                .permitAll()
+//                .and()
+//                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
+//                .and()
+//                .logout().permitAll();
 
         http.headers().frameOptions().sameOrigin();
 
@@ -46,6 +60,10 @@ public class SecurityConfiguration {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ Java Spring With User login and Registration Postgres SQL/code/src/main/java/com/example/demo/security/TokenManager.java
@@ -0,0 +1,40 @@
+package com.example.demo.security;
+
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims; import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class TokenManager implements Serializable {
+    private static final long serialVersionUID = 7008375124389347049L;
+    public static final long TOKEN_VALIDITY = 10 * 60 * 60; @Value("${secret}")
+    private String jwtSecret;
+    public String generateJwtToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        return Jwts.builder().setClaims(claims).setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
+                .signWith(key, SignatureAlgorithm.HS512).compact();
+    }
+    public Boolean validateJwtToken(String token, UserDetails userDetails) {
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token).getBody();
+        Boolean isTokenExpired = claims.getExpiration().before(new Date());
+        return (claims.getSubject().equals(userDetails.getUsername()) && !isTokenExpired);
+    }
+}
